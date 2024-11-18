c1955aca2d7f6a36eba4e75ea8f4594db2a533f6
Noah Miyabayashi <ac3po6@gmail.com>
2024-11-18 14:37:50 +0900

    reset

--- server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, Node.js!');
-}).listen(8080, () => {
-  console.log('Server running at http://localhost:8080/');
-});
